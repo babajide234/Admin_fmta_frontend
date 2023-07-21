45064ea7889f9c2ab547b2ddd1a865921949574a
babajide234 <babajide234@gmail.com>
2023-07-21 12:48:42 +0100

    get

--- src/components/navigation/Sidebar.jsx
+++ src/components/navigation/Sidebar.jsx
@@ -4,7 +4,8 @@ import { ReactComponent as Home } from "../../assets/main/icon/home.svg";
 import { ReactComponent as Shopping_cart } from "../../assets/main/icon/shopping-cart3.svg";
 import { ReactComponent as Box } from "../../assets/main/icon/box2.svg";
 import { ReactComponent as User } from "../../assets/main/icon/user2.svg";
-import { ReactComponent as Heart } from "../../assets/main/icon/Heart4.svg";
+// import { ReactComponent as Heart } from "../../assets/main/icon/Heart4.svg";
+
 import PropTypes from "prop-types";
 import { ListTree, Plus } from "lucide-react";
 
@@ -16,14 +17,13 @@ const Sidebar = () => {
         to={item.url}
         className={(props) => {
           return props.isActive ? "active-link" : "";
-        }}
-      >
+        }}>
         <div className={`hospitalSideNav__ul-listItem `}>
           {item.icon === "Home" && <Home />}
           {item.icon === "Shopping_cart" && <Shopping_cart />}
           {item.icon === "Box" && <Box />}
           {item.icon === "User" && <User />}
-          {item.icon === "Heart" && <Heart />}
+          {item.icon === "Heart" && <User />}
           {item.icon === "Plus" && <Plus />}
           {item.icon === "List" && <ListTree />}
           <span className="hospitalSideNav__ul-span">{item.name}</span>
